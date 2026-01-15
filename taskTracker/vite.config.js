182ed1f10eb46ef398315099c25d644e456a66c2
beripot <mjmaglangit03@gmail.com>
2026-01-15 22:44:07 +0800

    final touches (jk)

--- taskTracker/vite.config.js
+++ taskTracker/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/YOUR_REPOSITORY_NAME/', // e.g., /efficiency-tracker/
+  // Replace 'taskTracker' with your EXACT GitHub repository name
+  base: '/taskTracker/', 
 })
